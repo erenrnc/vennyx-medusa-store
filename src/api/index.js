4a3e56952e990e8b8b8a6df74440bba6c72a5c12
Eren <Johnny@DESKTOP-T0FNL93>
2024-10-11 17:06:54 +0300

    + added model + get detail by name

--- src/api/index.js
+++ src/api/index.js
@@ -14,7 +14,21 @@ export default () => {
 
         const postService = req.scope.resolve("postService");
 
-        const product = await postService.getMessage();
+        const limit = parseInt(req.query.limit) || 20;
+        const offset = parseInt(req.query.offset) || 0;
+
+        const product = await postService.getMessage(limit, offset);
+
+        res.json(JSON.parse(product));
+    })
+
+    router.get("/getby", async (req, res) => {
+
+        const postService = req.scope.resolve("postService");
+
+        const name = req.query.name;
+
+        const product = await postService.getPokeInfo(name);
 
         res.json(JSON.parse(product));
     })
--- src/services/post.ts
+++ src/services/post.ts
@@ -1,13 +1,34 @@
 ﻿import { TransactionBaseService } from "@medusajs/medusa"
+import Pokemon  from "../models/pokemonModel";
 
 const axios = require("axios");
 
 class PostService extends TransactionBaseService {
-    async getMessage() {
+    async getMessage(limit: number, offset: number) {
         try {
             // Dış API'ye GET isteği yapılıyor
-            const response = await axios.get("https://pokeapi.co/api/v2/ability/?limit=20&offset=20");
+            const response = await axios.get(`https://pokeapi.co/api/v2/ability/?limit=${limit}&offset=${offset}`);
             //console.log(response.data);
+            return JSON.stringify(response.data);
+        } catch (error) {
+            // Hata durumunda mesaj
+            //console.error("Dış API'den veri çekilemedi:", error);
+            //throw new Error("Veri çekilemedi");
+        }
+
+
+        return `veriyi çekemeyenler!`
+    }
+
+    async getPokeInfo(name: string) {
+        try {
+            // Dış API'ye GET isteği yapılıyor
+            const response = await axios.get(`https://pokeapi.co/api/v2/ability/${name}`);
+
+            // API'den dönen veriyi Pokemon modeline göre yapılandırıyoruz
+            //const pokemon = response.data.results.map(pokemonData => {
+            //    return new Pokemon(pokemonData.name, pokemonData.generation.url, pokemonData.id, pokemonData.is_main_series);
+            //});
             return JSON.stringify(response.data);
         } catch (error) {
             // Hata durumunda mesaj
